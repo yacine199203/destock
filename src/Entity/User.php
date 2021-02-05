a51f6d83474a48f522fad9d40a6f34758c98fcd8
BOUADJENEK Yacine <bouadjenek.yacin@gmail.com>
2021-02-05 10:19:18 +0100

    correction bugs

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -69,6 +69,22 @@ class User implements UserInterface
      */
     private $commandes;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Ce champ est vide")
+     */
+    private $adress;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $city;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $phone;
+
     public function __construct()
     {
         $this->commandes = new ArrayCollection();
@@ -214,6 +230,42 @@ class User implements UserInterface
                 $commande->setUser(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getAdress(): ?string
+    {
+        return $this->adress;
+    }
+
+    public function setAdress(string $adress): self
+    {
+        $this->adress = $adress;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(?string $phone): self
+    {
+        $this->phone = $phone;
 
         return $this;
     }
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -10,6 +10,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class UserType extends AbstractType
@@ -28,6 +29,71 @@ class UserType extends AbstractType
             'attr'=>[
                 'placeholder'=>'Nom'    
             ]
+        ])
+        ->add('adress',TextType::class,[
+            'label'=>'Adresse :',
+            'attr'=>[
+                'placeholder'=>'adresse'    
+            ]
+        ])
+        ->add('city',ChoiceType::class,[
+            'choices' => [
+               'Adrar' => 'Adrar',
+               'Chlef' => 'Chlef',
+               'Laghouat' => 'Laghouat',
+               'Oum El Bouaghi' => 'Oum El Bouaghi',
+               'Batna' => 'Batna',
+               'Béjaïa' => 'Béjaïa',
+               'Biskra' => 'Biskra',
+               'Béchar' => 'Béchar',
+               'Blida' => 'Blida',
+               'Bouira' => 'Bouira',
+               'Tamanrasset' => 'Tamanrasset',
+               'Tébessa' => 'Tébessa',
+               'Tlemcen' => 'Tlemcen',
+               'Tiaret' => 'Tiaret',
+               'Tizi Ouzou' => 'Tizi Ouzou',
+               'Alger' => 'Alger',
+               'Djelfa' => 'Djelfa',
+               'Jijel' => 'Jijel',
+               'Sétif' => 'Sétif',
+               'Saïda' => 'Saïda',
+               'Skikda' => 'Skikda',
+               'Sidi Bel Abbès' => 'Sidi Bel Abbès',
+               'Annaba' => 'Annaba',
+               'Guelma' => 'Guelma',
+               'Constantine' => 'Constantine',
+               'Médéa' => 'Médéa',
+               'Mostaganem' => 'Mostaganem',
+               'M\'Sila' => 'M\'Sila',
+               'Mascara' => 'Mascara',
+               'Ouargla' => 'Ouargla',
+               'Oran' => 'Oran',
+               'El Bayadh' => 'El Bayadh',
+               'Illizi' => 'Illizi',
+               'BBA' => 'BBA',
+               'Boumerdès' => 'Boumerdès',
+               'Tarf' => 'Tarf',
+               'Tindouf' => 'Tindouf',
+               'Tissemsilt' => 'Tissemsilt',
+               'El Oued' => 'El Oued',
+               'Khenchela' => 'Khenchela',
+               'Souk Ahras' => 'Souk Ahras',
+               'Tipaza' => 'Tipaza',
+               'Mila' => 'Mila',
+               'Aïn Defla' => 'Aïn Defla',
+               'Naâma' => 'Naâma',
+               'Aïn Témouchent' => 'Aïn Témouchent',
+               'Ghardaïa' => 'Ghardaïa',
+               'Relizane' => 'Relizane',
+                ]
+            ])
+        ->add('phone',NumberType::class,[
+            'label'=>'Téléphone :',
+            'attr'=>[
+                'placeholder'=>'tél'    
+            ],
+            'required'=> false,
         ])
         ->add('email',EmailType::class,[
             'label'=>'Email :',
